8a379c0f84110dda40ea2c8cf8bcf20c3a7f0b6a
Sara De Garmendia <109221595+Margaritasdg@users.noreply.github.com>
2023-03-16 09:14:26 -0300

    cambios

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,15 +2,19 @@ const express = require('express');
 const homeRoutes = require('./home.js');
 const userRoutes = require('./user.js');
 const productRoutes = require('./product.js');
-const categoryRoutes = require('./category.js')
-const orderRoutes = require('./order.js')
+const categoryRoutes = require('./category.js');
+const orderRoutes = require('./order.js');
+const shoppingCartRoutes = require ('./shoppingCart.js');
+const commentsRaitingRoutes = require ('./commentsRaiting.js');
 
 const app = express();
 
 app.use('/', homeRoutes);
 app.use('/user', userRoutes);
 app.use('/product', productRoutes);
-app.use('/category', categoryRoutes)
-app.use('/order', orderRoutes)
+app.use('/category', categoryRoutes);
+app.use('/order', orderRoutes);
+app.use('/shoppingCart',shoppingCartRoutes);
+app.use('/commentsRaiting',commentsRaitingRoutes);
 
 module.exports = app;
