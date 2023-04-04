9b0e651a8a6d182ab7ed4a2b25c41ab0b23a5b18
JulioXC4 <danielc4nt@gmail.com>
2023-04-03 19:31:20 -0500

    HOS_BACK Update

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -9,34 +9,34 @@ const createInitialData = async () => {
     //CREATE USERS
     await Promise.all(
         data.users.map(async (user) => {
-            await axios.post(`/user/createUser`, user)
+            await axios.post(`${HOST_BACK}/user/createUser`, user)
         })
     )
 
     //CREATE TABLETS
     await Promise.all(
         data.products[0]["Tablets"].map(async (tablet) => {
-            await axios.post(`https://karma-backend-production.up.railway.app/tablet/createTablet`, tablet)
+            await axios.post(`${HOST_BACK}/tablet/createTablet`, tablet)
         })
     )
 
     //CREATE LAPTOPS
     await Promise.all(
         data.products[1]["Laptops"].map(async (laptop) => {
-            await axios.post(`https://karma-backend-production.up.railway.app/laptop/createLaptop`, laptop)
+            await axios.post(`${HOST_BACK}/laptop/createLaptop`, laptop)
         })
     )
      //CREATE CELLPHONE
  await Promise.all(
     data.products[2]["CellPhone"].map(async (cellphone) => {
-        await axios.post(`https://karma-backend-production.up.railway.app/cellPhone/createCellPhone`, cellphone)
+        await axios.post(`${HOST_BACK}/cellPhone/createCellPhone`, cellphone)
     })
 )
 
     //CREATE TELEVISORES
     await Promise.all(
         data.products[3]["Television"].map(async(tv) =>{
-            await axios.post(`https://karma-backend-production.up.railway.app/tv/createTelevision`, tv)
+            await axios.post(`${HOST_BACK}/tv/createTelevision`, tv)
         })
     )
 }
