fe8415f6427bd53c1fa30172f66d4bfbd2683dfe
JulioXC4 <danielc4nt@gmail.com>
2023-04-03 19:14:12 -0500

    Agregado HOST_BACK y https a la funcion initialDat

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -16,27 +16,27 @@ const createInitialData = async () => {
     //CREATE TABLETS
     await Promise.all(
         data.products[0]["Tablets"].map(async (tablet) => {
-            await axios.post(`/tablet/createTablet`, tablet)
+            await axios.post(`https://${HOST_BACK}/tablet/createTablet`, tablet)
         })
     )
 
     //CREATE LAPTOPS
     await Promise.all(
         data.products[1]["Laptops"].map(async (laptop) => {
-            await axios.post(`/laptop/createLaptop`, laptop)
+            await axios.post(`https://${HOST_BACK}/laptop/createLaptop`, laptop)
         })
     )
      //CREATE CELLPHONE
  await Promise.all(
     data.products[2]["CellPhone"].map(async (cellphone) => {
-        await axios.post(`/cellPhone/createCellPhone`, cellphone)
+        await axios.post(`https://${HOST_BACK}/cellPhone/createCellPhone`, cellphone)
     })
 )
 
     //CREATE TELEVISORES
     await Promise.all(
         data.products[3]["Television"].map(async(tv) =>{
-            await axios.post(`/tv/createTelevision`, tv)
+            await axios.post(`https://${HOST_BACK}/tv/createTelevision`, tv)
         })
     )
 }
