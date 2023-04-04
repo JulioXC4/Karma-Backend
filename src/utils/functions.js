339672c763d3e1885e4dac93ab225334c05383fa
JulioXC4 <danielc4nt@gmail.com>
2023-04-03 19:07:14 -0500

    Removido el HOST_BACK de main para prueba

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -9,34 +9,34 @@ const createInitialData = async () => {
     //CREATE USERS
     await Promise.all(
         data.users.map(async (user) => {
-            await axios.post(`${HOST_BACK}/user/createUser`, user)
+            await axios.post(`/user/createUser`, user)
         })
     )
 
     //CREATE TABLETS
     await Promise.all(
         data.products[0]["Tablets"].map(async (tablet) => {
-            await axios.post(`${HOST_BACK}/tablet/createTablet`, tablet)
+            await axios.post(`/tablet/createTablet`, tablet)
         })
     )
 
     //CREATE LAPTOPS
     await Promise.all(
         data.products[1]["Laptops"].map(async (laptop) => {
-            await axios.post(`${HOST_BACK}/laptop/createLaptop`, laptop)
+            await axios.post(`/laptop/createLaptop`, laptop)
         })
     )
      //CREATE CELLPHONE
  await Promise.all(
     data.products[2]["CellPhone"].map(async (cellphone) => {
-        await axios.post(`${HOST_BACK}/cellPhone/createCellPhone`, cellphone)
+        await axios.post(`/cellPhone/createCellPhone`, cellphone)
     })
 )
 
     //CREATE TELEVISORES
     await Promise.all(
         data.products[3]["Television"].map(async(tv) =>{
-            await axios.post(`${HOST_BACK}/tv/createTelevision`, tv)
+            await axios.post(`/tv/createTelevision`, tv)
         })
     )
 }
