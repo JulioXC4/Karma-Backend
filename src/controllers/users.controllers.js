1d8bf7f631d7c6353867b153f41ec43b2299c8bc
Sara De Garmendia <109221595+Margaritasdg@users.noreply.github.com>
2023-04-09 22:44:03 -0300

    cambios

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1,7 +1,7 @@
 const { default: isEmail } = require('validator/lib/isEmail.js');
 const {User} = require('../db.js');
 const {regexPhoneNumber} = require('../utils/consts.js')
-const emailer = require ('../utils/emailer');
+const {sendMail,sendConfirmationRegistrationEmail} = require ('../utils/emailer');
     const createUser = async (req, res) => {
 
         try {
@@ -308,7 +308,7 @@ const emailer = require ('../utils/emailer');
             address: "none" 
           
           })
-          emailer.sendMail({email:email});
+         await sendConfirmationRegistrationEmail({ email });
           return res.status(200).send(`El usuario con la id ${id} fue creado correctamente`)
 
         } else {
--- src/utils/emailer.js
+++ src/utils/emailer.js
@@ -1,34 +1,49 @@
 const nodemailer = require ('nodemailer');
-//const htmlTemplate = require('../Template/htmlTemplate.js');
 const nodemailerSendgrid = require('nodemailer-sendgrid');
 require('dotenv').config();
 const {  SENDGRID_API_KEY} = process.env;
 
 
 const createTrans = () =>{
-
-    const transport = nodemailer.createTransport(
-    nodemailerSendgrid({
-      apiKey: SENDGRID_API_KEY,
-    })
-    );
+const transport = nodemailer.createTransport(nodemailerSendgrid({
+      apiKey: SENDGRID_API_KEY}));
     return transport;
 };
   
 
-const sendMail = async ({email}) => {
-    const transporter =  createTrans()
-    const info = await transporter.sendMail({
-        from:'"Karma" <karma.contact12@gmail.com>',
-          to:`${email}`,
-        subject:`Hola !  Bienvenido a KARMA`,
-        templateId: 'd-0e8b52137ae34984bce638e224dc8a3b',
-    });
-    console.log("message send :%s", info.messageId);
-    return
+const sendMail = async ({ email, subject, templateId }) => {
+  const transporter = createTrans()
+  const info = await transporter.sendMail({
+    from: '"Karma" <karma.contact12@gmail.com>',
+    to: email,
+    subject: subject,
+    templateId: templateId,
+  });
+  console.log("message send :%s", info.messageId);
+  return
+};
+
+const sendConfirmationRegistrationEmail = async ({ email }) => {
+  await sendMail({
+    email: email,
+    subject: 'Bienvenido a Karma',
+    templateId: 'd-0e8b52137ae34984bce638e224dc8a3b'
+  });
+};
+
+const sendPaymentConfirmationEmail = async ({ email }) => {
+  await sendMail({
+    email: email,
+    subject: 'Confirmación de pago en Karma',
+    templateId: 'd-9ac7a421d70a4f94b91d4c4d4e4a0196'
+  });
 }
 
-exports.sendMail= sendMail
+module.exports = {
+  sendMail,
+  sendConfirmationRegistrationEmail,
+  sendPaymentConfirmationEmail
+}
 
 
 
@@ -44,3 +59,19 @@ subject: `Hola ${newUser.name}, bienvenido a KARMA`,
           //  pass: "163536d728e867"
         //}
     //});
+    /*const approvedPaymentMercadoPago = async (req, res) => {
+
+  const {merchant_order_id, collection_status} = req.query
+  const merchantData = await getMerchantOrder(merchant_order_id)
+  const orderId = parseInt(merchantData.external_reference)
+
+  if(merchantData.status === 'closed' && collection_status === 'approved'){
+
+    cancelTimer(orderId)
+    await ChangeOrderStatus(orderId, "Orden Pagada")
+    await emptyUserShoppingCart(orderId)
+    const email = req.user.email; // correo del usuario logueado
+    await sendPaymentConfirmationEmail({ email });
+  }
+  return res.redirect(`${HOST_FRONT}/profile/orders`);
+}*/
